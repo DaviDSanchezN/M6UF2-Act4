36e4d793651ef2bf66ed3cb60a1dfda624a22d90
DaviDSanchezN <158029135+DaviDSanchezN@users.noreply.github.com>
2024-02-13 09:17:15 +0100

    Update preparat-1.sh

--- preparat-1.sh
+++ preparat-1.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
+# Escriu un script que comprovi l'estat d’un ventall de servidors remots, que es comprovi l'estat de cada servidor i que es registri el resultat en un arxiu. 
+# S’ha d'optimitzar el contingut de l’script a partir de l’ús de funcions. Opcionalment, pots usar Here-Docs i el Case.
+
 # Función para comprobar el estado de un servidor remoto y registrar el resultado
 check_server_status() {
-    local server=$1
-    local status
+    local server=$1  # Guarda el nombre del servidor pasado como argumento en la variable local 'server'
+    local status # Declara una variable local 'status' para almacenar el estado del servidor
 
     # Realizar la comprobación del estado del servidor mediante un ping
     if ping -c 1 "$server" &> /dev/null; then
